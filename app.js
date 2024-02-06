ac29a4209071bc5af8717f977be53c7cac8d8b29
Yanako Lajara <127878396+yanakolajara@users.noreply.github.com>
2024-02-06 13:44:02 -0500

    header

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
 const usersController = require("./usersControllers");
+const cors = require("cors");
 
 app.get("/", (req, res) => {
   res.send("This is an app my dude.");
+});
+
+app.use(cors({ origin: "*" }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Controll-Allow-Headers",
+    "Origin, X-Requrested-With, Content-Type, Accept"
+  );
+  next();
 });
 
 app.use("/users", usersController);
